fbdabcf5470bfd13f998df7368eec6edb98050f9
Matthew Foster <mjfoster126@gmail.com>
2020-12-13 21:38:01 -0600

    Created tableView and custom tableView cell for main viewController.  Added UIView extension to anchor elements correctly inside tableView Cell

--- GitHubSwift/Controller/ViewController.swift
+++ GitHubSwift/Controller/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let tableview = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.frame = view.frame
+        tableview.register(commitCell.self, forCellReuseIdentifier: "commitCell")
+        view.addSubview(tableview)
     }
 
 
+}
+
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "commitCell", for: indexPath) as! commitCell
+        
+        cell.author.text = "Text"
+        cell.dateTime.text = "13th Dec 2020"
+        cell.sha.text = "35fdc750232d9af45ffd9c657ab0db6e800c3a9d"
+        cell.commitDescription.text = "This is where the description will go so that it can be viewed."
+        return cell
+    }
+    
+    
+    
 }
 
---
+++ GitHubSwift/View/commitCell.swift
@@ -0,0 +1,83 @@
+//
+//  commitCell.swift
+//  GitHubSwift
+//
+//  Created by Matthew Foster on 13/12/20.
+//
+
+import UIKit
+
+class commitCell: UITableViewCell {
+    
+    let author: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 20.0, weight: UIFont.Weight.bold)
+        return lbl
+    }()
+    let dateTime: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 16.0, weight: UIFont.Weight.regular)
+        return lbl
+    }()
+    let sha: UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .lightGray
+        lbl.font = UIFont.systemFont(ofSize: 16.0, weight: UIFont.Weight.black)
+        lbl.adjustsFontSizeToFitWidth = true
+        return lbl
+    }()
+    let userImage: UIImageView = {
+        let img = UIImageView()
+        img.backgroundColor = .brown
+        img.layer.cornerRadius = 25
+        return img
+    }()
+    let commitDescription: UITextView = {
+        let tv = UITextView()
+        tv.isEditable = false
+        tv.translatesAutoresizingMaskIntoConstraints = true
+        tv.sizeToFit()
+        tv.isScrollEnabled = false
+        tv.font = UIFont.systemFont(ofSize: 18.0, weight: UIFont.Weight.light)
+        return tv
+    }()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String!)
+    {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(userImage)
+        addSubview(author)
+        addSubview(dateTime)
+        addSubview(sha)
+        addSubview(commitDescription)
+        
+        userImage.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: 50, height: 50, enableInsets: false)
+        
+        author.anchor(top: topAnchor, left: userImage.rightAnchor, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: frame.size.width - userImage.frame.width, height: 25, enableInsets: false)
+        
+        dateTime.anchor(top: author.bottomAnchor, left: userImage.rightAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: frame.size.width - userImage.frame.width, height: 25, enableInsets: false)
+        
+        commitDescription.anchor(top: userImage.bottomAnchor, left: userImage.rightAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 6, paddingBottom: 10, paddingRight: 0, width: frame.size.width - userImage.frame.width - 10, height: 0, enableInsets: false)
+        
+        sha.anchor(top: commitDescription.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 10, paddingRight: 10, width: frame.size.width, height: 0, enableInsets: false)
+         
+       
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)!
+    }
+
+}
